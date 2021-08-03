9049718baeb45ebbc855a9b96faab0dd87ca1d89
Wsaeauiwawa <wathinee477@gmail.com>
2021-08-03 23:32:57 +0700

    updated DB

--- server.js
+++ server.js
@@ -65,6 +65,33 @@ app.get('/herb/:id', (req, res) => {
     }
 })
 
+//retrieve herb name by name
+// app.get('/herb/:common_name', (req, res) => {
+//     let common_name = req.params.common_name;
+
+//     if(!common_name) {
+//         return res.status(400).send({ error: true, message: "Please provide herb id"});
+//     }
+//     else
+//     {
+//         dbCon.query("SELECT * FROM name WHERE common_name = ?" ,common_name ,(error, results, fields) => {
+//             if (error) throw error;
+
+//             let message = "";
+//             if (results === undefined || results.length == 0) {
+//                 message = "herb not found";
+//             }
+//             else
+//             {
+//                 message = "Successfully retrieved herb data";
+//             }
+
+//             return res.send({error: false, detail: results[0], message: message});
+
+//         })
+//     }
+// })
+
 
 
 app.listen(3000, () => {
